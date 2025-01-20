72258d453d8c90f51581c0bde54710cf3e5d9430
Alejandro Alvarez <zorro201021@hotmail.com>
2025-01-20 15:16:37 -0300

    Implementacion de informacion independiente

---
+++ assets/js/principal.js
@@ -0,0 +1,12 @@
+// Se ejecuta al cargar la pagina
+$(window).on('load',function() {
+    // Obtiene e implamenta la inforamcion en el json.
+    fetch('../info.json').then(
+        response => response.json()).then(
+            data => {
+                document.getElementById('cant_empresas').innerHTML= data.cant_empresas + "+";
+                document.getElementById('cant_clientes').innerHTML= data.cant_clientes + "+";
+                document.getElementById('cant_proyectos').innerHTML= data.cant_proyectos + "+";
+            }
+        ).catch(error => console.error("Error al recuperar informacion del JSON: ", error));
+});
